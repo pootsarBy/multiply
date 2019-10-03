59ba7e0b77118b8b20caf3aaee67c65e5747a4ab
Mikhail Rybinski <pootsar@gmail.com>
2019-10-03 21:16:05 +0300

    refactor : add "", prettier

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
 module.exports = function multiply(first, second) {
-  let arrFirst = first.split('').reverse();
-  let arrSecond = second.split('').reverse();
+  let arrFirst = first.split("").reverse();
+  let arrSecond = second.split("").reverse();
 
   console.log(first);
   console.log(second);
   let stack = [];
-//умножаем значения в столбик
+  //умножаем значения в столбик
   for (let i = 0; i < arrFirst.length; i++) {
     for (let j = 0; j < arrSecond.length; j++) {
-      let powElem = arrFirst[i] * arrSecond[j];  //умножаем элементы двух массивов
-      if (stack[i+j])         //проверка стека на наличие элемента
-      stack[i + j] += powElem;  //если элемент есть, то прибавляем к нему 
-      else                    //если нет
-      stack[i + j] = powElem;   //добавляем новый элемент
-     
+      let powElem = arrFirst[i] * arrSecond[j]; //умножаем элементы двух массивов
+      if (stack[i + j])
+        //проверка стека на наличие элемента
+        stack[i + j] += powElem;
+      //если элемент есть, то прибавляем к нему
+      //если нет
+      else stack[i + j] = powElem; //добавляем новый элемент
     }
   }
 
   for (let i = 0; i < stack.length; i++) {
-    let num = stack[i] % 10;                //
+    let num = stack[i] % 10; //
     let move = Math.floor(stack[i] / 10);
     stack[i] = num;
-
-    if (stack[i + 1])
-      stack[i + 1] += move;  //прибавляем значение в ячейку выше
-    else if (move != 0)         //если ячейки закончились
-      stack[i + 1] = move;      //добавляем новые
+    if (stack[i + 1]) stack[i + 1] += move;
+    //прибавляем значение в ячейку выше
+    else if (move != 0)
+      //если ячейки закончились
+      stack[i + 1] = move; //добавляем новые
   }
 
-
-  return stack.reverse().join('');  //приводим строку в нормальный вид
-}
+  return stack.reverse().join(""); //приводим строку в нормальный вид
+};
